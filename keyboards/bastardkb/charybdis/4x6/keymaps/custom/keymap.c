e6d5c6f64834b2d6d7a1323943595917b4b6d680
ALNOR-HPC\alnor <alxrsk@gmail.com>
2025-01-26 10:06:48 +0300

    game layers

--- keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
@@ -22,6 +22,8 @@
 
 enum charybdis_keymap_layers {
     LAYER_BASE = 0,
+    LAYER_GAME1,
+    LAYER_GAME2,
     LAYER_LOWER,
     LAYER_RAISE,
     LAYER_POINTER,
@@ -29,7 +31,8 @@ enum charybdis_keymap_layers {
 
 enum custom_keycodes {
     KC_QWERTY = QK_USER,
-    KC_COLEMAK,
+    KC_GAME1,
+    KC_GAME2,
     KC_PRVWD,
     KC_NXTWD,
     KC_LSTRT,
@@ -81,6 +84,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
+  [LAYER_GAME1] = LAYOUT(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RBRC,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+        KC_ESC,    KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+        KC_TAB,    KC_G,    KC_A,    KC_S,    KC_D,    KC_F,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_LSFT,    KC_B,    PT_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RSFT,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                  KC_LCTL, KC_LALT,  KC_SPC,     KC_ENT, KC_RCTL,
+                                           KC_LGUI,   LOWER,      RAISE
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [LAYER_GAME2] = LAYOUT(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RBRC,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+        KC_ESC,    KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,   KC_UP,    KC_O,    KC_P, KC_LBRC,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+        KC_TAB,    KC_G,    KC_A,    KC_S,    KC_D,    KC_F,       KC_H, KC_LEFT, KC_DOWN,KC_RIGHT, KC_SCLN, KC_QUOT,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_LSFT,    KC_B,    PT_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RSFT,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                  KC_LCTL, KC_LALT,  KC_SPC,     KC_ENT, KC_RCTL,
+                                           KC_LGUI,   LOWER,      RAISE
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
   [LAYER_LOWER] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
@@ -113,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_POINTER] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,  EE_CLR,
+       QK_BOOT,  EE_CLR, XXXXXXX,KC_GAME2,KC_GAME1,KC_QWERTY, KC_QWERTY,KC_GAME1,KC_GAME2, XXXXXXX, QK_BOOT,  EE_CLR,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
@@ -131,6 +164,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_QWERTY:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(LAYER_BASE);
+            }
+            return false;
+        case KC_GAME1:
+            if (record->event.pressed) {
+                set_single_default_layer(LAYER_GAME1);
+            }
+            return false;
+        case KC_GAME2:
+            if (record->event.pressed) {
+                set_single_default_layer(LAYER_GAME2);
             }
             return false;
         case KC_PRVWD:
