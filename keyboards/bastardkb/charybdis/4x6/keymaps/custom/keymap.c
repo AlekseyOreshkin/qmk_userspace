b8fe66f4f7b4a861c8526c267485c382e0eecb6a
ALNOR-HPC\alnor <alxrsk@gmail.com>
2025-02-07 08:23:17 +0300

    swap level modifiers

--- keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_LCTL, KC_LALT,  KC_SPC,     KC_ENT, KC_RCTL,
-                                           KC_LGUI,   LOWER,      RAISE
+                                  KC_LCTL,   LOWER,  KC_SPC,     KC_ENT,   RAISE,
+                                           KC_LGUI, KC_LALT,    KC_RCTL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
